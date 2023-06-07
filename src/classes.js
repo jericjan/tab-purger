da05d92c08e931cf7a637790cc97dd2ca0ce023d
jericjan <cantilfrederick@gmail.com>
2023-06-07 13:12:10 +0800

    prettified js and added dist folder

--- src/classes.js
+++ src/classes.js
@@ -1,149 +1,145 @@
 class Tab {
+  /**
+   * For managing a tab and the HTML associated with it.
+   * @param {HTMLLIElement} element - this is the HTML element that displays the tab
+   * @param {chrome.tabs.Tab} tab - this is the actual tab object
+   * @param {chrome.tabs.Tab} currentTab - the currently active tab
+   */
+  constructor(element, tab, currentTab) {
+    this.element = element;
+    this.tab = tab;
+    this.currentTab = currentTab;
+  }
 
-    /**
-     * For managing a tab and the HTML associated with it.       
-     * @param {HTMLLIElement} element - this is the HTML element that displays the tab
-     * @param {chrome.tabs.Tab} tab - this is the actual tab object
-     * @param {chrome.tabs.Tab} currentTab - the currently active tab
-     */
-    constructor(element, tab, currentTab) {
-      this.element = element;
-      this.tab = tab;
-      this.currentTab = currentTab
+  /** returns the "a" element associated with the tab */
+  get aElem() {
+    return this.element.querySelector("a");
+  }
+
+  /** returns the close button element of the tab */
+  get closeButton() {
+    return this.element.querySelector(".close");
+  }
+
+  /** returns the tab's unique ID, not to be confused with the element's own ID*/
+  get id() {
+    return this.tab.id;
+  }
+
+  /** Sets the title, URL, icon, and ID of the HTML element*/
+  setContents() {
+    const title = this.tab.title.trim();
+    const pathname = this.tab.url;
+    const faviUrl = this.tab.favIconUrl;
+    this.element.querySelector(".title").textContent = title;
+    this.element.querySelector(".pathname").textContent = pathname;
+    this.element.querySelector(".iconDiv > img").src = faviUrl;
+    this.element.querySelector("a").setAttribute("id", `tab-${this.tab.id}`);
+  }
+
+  selectTab() {
+    // unselects all tabs
+    var selectedTabList = document.querySelectorAll(".selected");
+    selectedTabList.forEach((element) => {
+      element.classList.remove("selected");
+    });
+
+    // selects this tab
+    this.element
+      .querySelector(`#tab-${this.tab.id}`)
+      .parentElement.classList.add("selected");
+  }
+
+  async focusWindowTab() {
+    await chrome.tabs.update(this.tab.id, {
+      active: true,
+    });
+    await chrome.windows.update(this.tab.windowId, {
+      focused: true,
+    });
+  }
+
+  get isCurrentTab() {
+    return this.currentTab.id == this.tab.id;
+  }
+
+  /** Checks if it's the current tab and if it is, gives it the appropriate class name */
+  markIfCurrentTab() {
+    if (this.isCurrentTab) {
+      this.element.classList.add("current-tab");
     }
-
-    /** returns the "a" element associated with the tab */
-    get aElem(){
-        return this.element.querySelector("a")
-    }
-
-    /** returns the close button element of the tab */
-    get closeButton(){
-        return this.element.querySelector(".close")
-    }
-
-    /** returns the tab's unique ID, not to be confused with the element's own ID*/
-    get id(){
-        return this.tab.id
-    }
-
-    /** Sets the title, URL, icon, and ID of the HTML element*/
-    setContents(){
-        const title = this.tab.title.trim();
-        const pathname = this.tab.url;
-        const faviUrl = this.tab.favIconUrl
-        this.element.querySelector(".title").textContent = title;
-        this.element.querySelector(".pathname").textContent = pathname;
-        this.element.querySelector(".iconDiv > img").src = faviUrl;
-        this.element.querySelector("a").setAttribute('id', `tab-${this.tab.id}`);        
-    }
-
-    selectTab(){
-
-        // unselects all tabs
-        var selectedTabList = document.querySelectorAll(".selected")
-        selectedTabList.forEach(element => {
-            element.classList.remove('selected')
-        });    
-
-        // selects this tab
-        this.element.querySelector(`#tab-${this.tab.id}`).parentElement.classList.add('selected')
-    }
-
-    async focusWindowTab(){
-
-        await chrome.tabs.update(this.tab.id, {
-            active: true
-        });        
-        await chrome.windows.update(this.tab.windowId, {
-            focused: true
-        });        
-    }
-
-    get isCurrentTab(){
-        return this.currentTab.id == this.tab.id
-    }
-
-    /** Checks if it's the current tab and if it is, gives it the appropriate class name */
-    markIfCurrentTab(){
-        if (this.isCurrentTab) {
-            this.element.classList.add('current-tab')
-        }
-    }
-
-}    
-
-
-class TabContainer {
-
-    /** For managing all Tab (not chrome.tabs.Tab) objects */
-    constructor(){        
-        /**
-         * A list of Tab objects.  
-         * Used for getting the IDs of the tabs in order to close them.
-         * @type {Tab[]}
-         */  
-        this.tabObjs = []
-    }
-
-    /**    
-     * @param {Tab} tabObj - gets added to {@link tabObjs} dict
-     * @param {HTMLLIElement} tabObj.element - gets added to {@link elems}
-     */
-    add(tabObj){        
-        this.tabObjs.push(tabObj)
-    }
-
-    /** @param {number} tabId */
-    async closeTab(tabId){
-        console.log(`Closing tab ${tabId}.`)    
-
-        const index = this.tabObjs.findIndex(function(item) {
-            return item.id === tabId;
-          });
-                
-        if (!this.tabObjs[index].isCurrentTab || confirm("This is the current tab! Are you sure you want to close it?")){
-            this.tabObjs.splice(index, 1)
-            await chrome.tabs.remove(tabId);
-            document.querySelector(`#tab-${tabId}`).parentElement.remove()    
-        } 
-    }
-
-    /** Displays all elements in `elems` by appending it to `ul` */
-    showAll(){
-
-        const getElems = function*() {            
-            for (const tabObj of this.tabObjs){  
-                yield tabObj.element
-            }
-        }.bind(this)
-
-        const uniqueElems = new Set([...getElems()]) //removes dupes just in case
-        document.querySelector("ul").append(...uniqueElems);
-    }
-
-    /** Clears all HTML elements associated with tabs w/o actually closing any tabs */
-    clearElems(){
-        document.querySelector("body > ul").innerHTML = ""
-
-        // empty this cuz it'll get set again anyway
-        this.tabObjs = []
-    }
-    
-    /** runs {@link closeTab()} on all currently displayed tabs */
-    async purgeDisplayed(){
-        const getIds = function*() {            
-            for (const tabObj of this.tabObjs){  
-                yield tabObj.id
-            }
-        }.bind(this)
-
-        for (const id of [...getIds()]){            
-            await this.closeTab(id)
-        }
-    }
-
+  }
 }
 
+class TabContainer {
+  /** For managing all Tab (not chrome.tabs.Tab) objects */
+  constructor() {
+    /**
+     * A list of Tab objects.
+     * Used for getting the IDs of the tabs in order to close them.
+     * @type {Tab[]}
+     */
+    this.tabObjs = [];
+  }
+
+  /**
+   * @param {Tab} tabObj - gets added to {@link tabObjs} dict
+   * @param {HTMLLIElement} tabObj.element - gets added to {@link elems}
+   */
+  add(tabObj) {
+    this.tabObjs.push(tabObj);
+  }
+
+  /** @param {number} tabId */
+  async closeTab(tabId) {
+    console.log(`Closing tab ${tabId}.`);
+
+    const index = this.tabObjs.findIndex(function (item) {
+      return item.id === tabId;
+    });
+
+    if (
+      !this.tabObjs[index].isCurrentTab ||
+      confirm("This is the current tab! Are you sure you want to close it?")
+    ) {
+      this.tabObjs.splice(index, 1);
+      await chrome.tabs.remove(tabId);
+      document.querySelector(`#tab-${tabId}`).parentElement.remove();
+    }
+  }
+
+  /** Displays all elements in `elems` by appending it to `ul` */
+  showAll() {
+    const getElems = function* () {
+      for (const tabObj of this.tabObjs) {
+        yield tabObj.element;
+      }
+    }.bind(this);
+
+    const uniqueElems = new Set([...getElems()]); //removes dupes just in case
+    document.querySelector("ul").append(...uniqueElems);
+  }
+
+  /** Clears all HTML elements associated with tabs w/o actually closing any tabs */
+  clearElems() {
+    document.querySelector("body > ul").innerHTML = "";
+
+    // empty this cuz it'll get set again anyway
+    this.tabObjs = [];
+  }
+
+  /** runs {@link closeTab()} on all currently displayed tabs */
+  async purgeDisplayed() {
+    const getIds = function* () {
+      for (const tabObj of this.tabObjs) {
+        yield tabObj.id;
+      }
+    }.bind(this);
+
+    for (const id of [...getIds()]) {
+      await this.closeTab(id);
+    }
+  }
+}
 
 export { Tab, TabContainer };
--- src/script.js
+++ src/script.js
@@ -1,88 +1,88 @@
 import { Tab, TabContainer } from "./classes.js";
 
+var inputBox = document.querySelector("input");
+inputBox.focus();
 
-var inputBox = document.querySelector("input")
-inputBox.focus()
-
-const tabCont = new TabContainer()
+const tabCont = new TabContainer();
 
 /** Counts all currently open tabs (not searched tabs) */
 async function updateTabCount() {
-    var allTabs = await chrome.tabs.query({});
-    document.querySelector("#tabCount").innerHTML = allTabs.length
+  var allTabs = await chrome.tabs.query({});
+  document.querySelector("#tabCount").innerHTML = allTabs.length;
 }
-await updateTabCount()
+await updateTabCount();
 
 /** @returns {Promise<chrome.tabs.Tab|undefined>} */
 async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };    
-    
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  let queryOptions = { active: true, lastFocusedWindow: true };
+
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
-/** 
- * Searches for tabs and displays them  
+/**
+ * Searches for tabs and displays them
  * Runs when the extension is opened, or when the user is typing on the search bar */
 async function doSearch() {
-    tabCont.clearElems()
-    var allTabs = await chrome.tabs.query({});
-    var currentTab = await getCurrentTab();
-    var search = document.querySelector("input").value.toLowerCase();
+  tabCont.clearElems();
+  var allTabs = await chrome.tabs.query({});
+  var currentTab = await getCurrentTab();
+  var search = document.querySelector("input").value.toLowerCase();
 
-    /** Filtered tabs based on search query. Uses a generator function that gets turned into a normal array. */
-    const searchedTabs = [...(function*() {
-        for (let tab of allTabs) {
-            if (tab.title.toLowerCase().includes(search) || tab.url.toLowerCase().includes(search)) {
-                yield tab
-            }
-        }})()
-    ]
+  /** Filtered tabs based on search query. Uses a generator function that gets turned into a normal array. */
+  const searchedTabs = [
+    ...(function* () {
+      for (let tab of allTabs) {
+        if (
+          tab.title.toLowerCase().includes(search) ||
+          tab.url.toLowerCase().includes(search)
+        ) {
+          yield tab;
+        }
+      }
+    })(),
+  ];
 
-    document.querySelector("#resultCount").innerHTML = searchedTabs.length
-    const collator = new Intl.Collator();
-    searchedTabs.sort((a, b) => collator.compare(a.title, b.title));
+  document.querySelector("#resultCount").innerHTML = searchedTabs.length;
+  const collator = new Intl.Collator();
+  searchedTabs.sort((a, b) => collator.compare(a.title, b.title));
 
-    const template = document.getElementById("li_template");    
-    
-    for (const tab of searchedTabs) {
+  const template = document.getElementById("li_template");
 
-        /** @type {HTMLLIElement} */
-        const element = template.content.firstElementChild.cloneNode(true);
+  for (const tab of searchedTabs) {
+    /** @type {HTMLLIElement} */
+    const element = template.content.firstElementChild.cloneNode(true);
 
-        const tabObj = new Tab(element, tab, currentTab)
+    const tabObj = new Tab(element, tab, currentTab);
 
-        tabObj.setContents()
+    tabObj.setContents();
 
-        tabObj.aElem.addEventListener("click", async (e) => {            
+    tabObj.aElem.addEventListener("click", async (e) => {
+      if (e.ctrlKey) {
+        // select tab
+        tabObj.selectTab();
+      } else {
+        // focus window as well as the active tab
+        await tabObj.focusWindowTab();
+      }
+    });
+    tabObj.closeButton.addEventListener("click", async (e) => {
+      e.stopPropagation();
+      await tabCont.closeTab(tabObj.id);
+      await updateTabCount();
+    });
 
-            if (e.ctrlKey) { // select tab            
-                tabObj.selectTab()                
-            } else { // focus window as well as the active tab
-                await tabObj.focusWindowTab()
-            }
-        });
-        tabObj.closeButton.addEventListener("click", async (e) => {            
-            e.stopPropagation();
-            await tabCont.closeTab(tabObj.id)
-            await updateTabCount()
+    tabObj.markIfCurrentTab();
 
-        });
+    tabCont.add(tabObj);
+  }
 
-        tabObj.markIfCurrentTab()        
+  tabCont.showAll();
 
-        tabCont.add(tabObj)
-    }
-    
-    tabCont.showAll()
-
-    await updateTabCount()
-
+  await updateTabCount();
 }
 
-
-
-await doSearch()
+await doSearch();
 
 /**
  * delays running a function until some time has passed
@@ -90,72 +90,81 @@ await doSearch()
  * @param {number} wait - how long to wait
  */
 function debounce(callback, wait) {
-    let timeout;
-    return (...args) => {
-        clearTimeout(timeout);
-        timeout = setTimeout(function () { callback.apply(this, args); }, wait);
-    };
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      callback.apply(this, args);
+    }, wait);
+  };
 }
 
-inputBox.addEventListener("keyup", debounce(async () => {
-    doSearch()
-}, 250))
-
+inputBox.addEventListener(
+  "keyup",
+  debounce(async () => {
+    doSearch();
+  }, 250)
+);
 
 const purgeButton = document.querySelector("button#purge");
 purgeButton.addEventListener("click", async () => {
+  await tabCont.purgeDisplayed();
 
-    await tabCont.purgeDisplayed()
-
-    await updateTabCount()
-
+  await updateTabCount();
 });
 
-document.addEventListener('keyup', function (e) {
+document.addEventListener("keyup", function (e) {
+  /**
+   * Navigates through the list of elements associated with the tabs
+   * If none is currently selected, select the first item
+   * If something is selected, unselect it, navigate up/down, and select the new item
+   * @param {string} direction - the direction to move towards
+   */
+  function navigate(direction) {
+    var tabList = document.querySelectorAll("body > ul > li");
+    var selectedItems = document.querySelectorAll(".selected");
 
-    /**
-     * Navigates through the list of elements associated with the tabs  
-     * If none is currently selected, select the first item  
-     * If something is selected, unselect it, navigate up/down, and select the new item
-     * @param {string} direction - the direction to move towards
-     */
-    function navigate(direction) {
+    if (selectedItems.length == 0) {
+      selectedItem = tabList[0];
+    } else {
+      var selectedItem = selectedItems[0];
+      selectedItem.classList.remove("selected");
 
-        var tabList = document.querySelectorAll("body > ul > li")
-        var selectedItems = document.querySelectorAll(".selected")
-
-        if (selectedItems.length == 0) {
-            selectedItem = tabList[0]
-        } else {
-            var selectedItem = selectedItems[0]
-            selectedItem.classList.remove('selected')
-
-            if (direction == 'up') {
-                selectedItem = (selectedItem.previousSibling != null) ? selectedItem.previousSibling : selectedItem
-            } else if (direction == 'down') {
-                selectedItem = (selectedItem.nextSibling != null) ? selectedItem.nextSibling : (selectedItem.previousSibling != null) ? selectedItem.previousSibling : selectedItem
-            }
-        }
-
-        if (selectedItem != undefined) {
-            selectedItem.classList.add('selected')
-            selectedItem.scrollIntoView()
-        }
-
+      if (direction == "up") {
+        selectedItem =
+          selectedItem.previousSibling != null
+            ? selectedItem.previousSibling
+            : selectedItem;
+      } else if (direction == "down") {
+        selectedItem =
+          selectedItem.nextSibling != null
+            ? selectedItem.nextSibling
+            : selectedItem.previousSibling != null
+            ? selectedItem.previousSibling
+            : selectedItem;
+      }
     }
 
-
-    if (document.activeElement != document.querySelector("input")) {
-        if (e.key == "k") { //Down      
-            navigate("down")
-        } else if (e.key == "j") { // Up
-            navigate("up")
-        } else if (e.key == "Delete") { // Delete
-            var selectedItems = document.querySelectorAll(".selected")
-            selectedItems.forEach(element => {
-                element.querySelector(".tabContainer > .closeDiv > button").click()
-            });
-            navigate("down")
-        }
+    if (selectedItem != undefined) {
+      selectedItem.classList.add("selected");
+      selectedItem.scrollIntoView();
     }
+  }
+
+  if (document.activeElement != document.querySelector("input")) {
+    if (e.key == "k") {
+      //Down
+      navigate("down");
+    } else if (e.key == "j") {
+      // Up
+      navigate("up");
+    } else if (e.key == "Delete") {
+      // Delete
+      var selectedItems = document.querySelectorAll(".selected");
+      selectedItems.forEach((element) => {
+        element.querySelector(".tabContainer > .closeDiv > button").click();
+      });
+      navigate("down");
+    }
+  }
 });
